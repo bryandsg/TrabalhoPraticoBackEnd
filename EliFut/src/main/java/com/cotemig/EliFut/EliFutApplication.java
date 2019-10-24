c2096d0d2312c00ef4e15e53b502ed025ed7ad87
71801227 <71801227@aluno.faculdadecotemig.br>
2019-10-23 22:00:51 -0300

    Estrutura Inicial do Projeto EliFut

---
+++ EliFut/src/main/java/com/cotemig/EliFut/EliFutApplication.java
@@ -0,0 +1,13 @@
+package com.cotemig.EliFut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliFutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliFutApplication.class, args);
+	}
+
+}
---
+++ EliFut/src/test/java/com/cotemig/EliFut/EliFutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotemig.EliFut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliFutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
